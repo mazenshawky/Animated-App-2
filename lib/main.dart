5739e120af5ae5270db444c21314a959bfdbb1d3
mazenshawky <mazenshawqy@gmail.com>
2025-06-19 15:12:06 +0300

    Add animated opacity example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animated_app_2/implicit_animations/animated_container_example.dart';
+import 'package:animated_app_2/implicit_animations/animated_opacity_example.dart';
 import 'package:animated_app_2/implicit_animations/animated_text_style_example.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_app_2/implicit_animations/animated_align_example.dart';
@@ -40,6 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
             _myButton("Animated Align Example", const AnimatedAlignExample()),
             _myButton("Animated Container Example", const AnimatedContainerExample()),
             _myButton("Animated Text Style Example", const AnimatedTextSizeExample()),
+            _myButton("Animated Opacity Example", const AnimatedOpacityExample()),
           ],
         ),
       ),
